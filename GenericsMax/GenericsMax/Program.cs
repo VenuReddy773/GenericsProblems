17eec122b00e07d488a0f9676f8d3d10cfff600c
“VenuReddy773” <venu706@gmail.com>
2021-11-18 21:15:43 +0530

    [VenuGopalReddy] Added: Maximum Of Three Numbers Feature

---
+++ GenericsMax/GenericsMax/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GenericsMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome To Program To Find Max Of Three\n");
+                Console.WriteLine("choose Option:\n 1.IntMax\n 2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter Three Numbers to Check Max.");
+                        int first = Convert.ToInt32(Console.ReadLine());
+                        int second = Convert.ToInt32(Console.ReadLine());
+                        int third = Convert.ToInt32(Console.ReadLine());
+                        int result = MaximumOfThree . MaxInt(first, second, third);
+                        Console.WriteLine(result +" is Maximum Of Three");
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+                
+            
+           
+        }
+    }
+}
+
+        
